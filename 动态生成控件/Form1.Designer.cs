d61d37428b45196ce68f3d871604e7c5f4f30219
admin <admin@DESKTOP-J0FJ8NJ>
2020-06-12 17:26:04 +0800

    添加项目文件。

---
+++ 动态生成控件/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace 动态生成控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(356, 376);
+            this.panel1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(67, 11);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(50, 33);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(190, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(50, 33);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ACTION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "row";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "cloumn";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(386, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 432);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "动态生成图片显示框";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 动态生成控件/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace 动态生成控件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.AutoScroll = true;//让panel显示滚动条           
+        }
+        public  string ImagePath = Application.StartupPath +"\\image\\";
+        string[,] ImageFromPath = new string[300, 300];
+        int Row = 3, Cloumn = 3;
+        public string AmpImagePath = string.Empty;
+        /// <summary>
+        /// 自动生成图片控件并加载图片，同时给图片控件添加双击事件
+        /// </summary>
+        /// <param name="row">生成图片控件的行数</param>
+        /// <param name="cloumn">生成图片控件的列数</param>
+        public void PictureControl(int row, int cloumn)
+        {
+            try
+            {
+               int ImageBoxCount = 0;
+               panel1.Controls.Clear();
+               for (int i = 0; i < row; i++)
+                    for (int j = 0; j < cloumn; j++)
+                    {
+                        PictureBox p = new PictureBox();
+                        p.Name = ImageBoxCount.ToString();
+                        p.Size = new System.Drawing.Size(108, 216);//长宽比例是1.25:1
+                        p.Top = i * 226;
+                        p.Left = j * 118;
+                        p.MouseDoubleClick += M;//鼠标双击事件
+                        ImageBoxCount++;
+                        p.BackColor = Color.Gray;
+                        p.SizeMode = PictureBoxSizeMode.Zoom;
+                        p.BorderStyle = BorderStyle.FixedSingle;
+                        if (IsFileInUse(ImageFromPath[i, j]) == false)
+                        {                           
+                            Stream s = File.Open(ImageFromPath[i, j], FileMode.Open);
+                            p.Image = Image.FromStream(s);
+                            s.Close();
+                            s.Dispose();
+                        }
+                        else
+                        {
+                            //p.Image = Image.FromFile(ImageFromPath[i, j]);
+                        }
+                        p.BackColor = Color.Gray;
+                        this.panel1.Controls.Add(p);
+                    }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+        private void M(object sender, EventArgs e)
+        {
+            try
+            {
+                PictureBox p = sender as PictureBox;
+                AmpImagePath = ImageFromPath[Convert.ToInt32(p.Name) / Cloumn,Convert.ToInt32(p.Name) % Cloumn];
+                Stream s = File.Open(AmpImagePath, FileMode.Open);
+                pictureBox1.Image = Image.FromStream(s);
+                s.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Row = int.Parse(richTextBox1.Text);
+            Cloumn = int.Parse(richTextBox2.Text);
+            for (int i = 0; i < Row; i++)
+                for (int j = 0; j < Cloumn; j++)
+                {
+                    ImageFromPath[i, j] = ImagePath + (i * Cloumn + j + 1).ToString() + ".jpg"; //保存每张图的路径
+                    //Console.WriteLine(ImagePath + (i * 3 + j + 1).ToString() + ".jpg");
+                }
+            PictureControl(Row, Cloumn);
+        }
+
+        public static bool IsFileInUse(string fileName)
+        {
+            bool inUse = true;
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
+                inUse = false;
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Close();
+            }
+            return inUse;//true表示正在使用,false没有使用
+        }
+
+
+    }
+}
